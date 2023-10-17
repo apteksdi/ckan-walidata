d513ef3c9f527e985177ae80e88fb5091d91971b
aptek sdi <91813373+apteksdi@users.noreply.github.com>
2023-10-17 10:25:58 +0700

    Update 00_harvest.sh

--- compose/00_harvest.sh
+++ compose/00_harvest.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 ckan -c /srv/app/production.ini harvester initdb
+ckan -c /srv/app/production.ini resourceauthorizer initdb
+ckan -c /srv/app/production.ini collaborator-orgs init-db
+ckan -c /srv/app/production.ini activitiestracker initdb
